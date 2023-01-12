8d88b474d04ae321821f166c9d81ec2ed04a2b11
Just-Akinyi <justakinyi1@gmail.com>
2023-01-12 15:35:35 +0300

    create user table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
 -- creates user table
+-- sql query to create table users
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
