af01cb055004726e5d931b42ddb331f4ce7353bd
Just-Akinyi <justakinyi1@gmail.com>
2023-01-13 00:01:27 +0300

    create user table and country

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
--- creates user table
--- sql query to create table users
-CREATE TABLE IF NOT EXISTS users (
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255)
 );
-
